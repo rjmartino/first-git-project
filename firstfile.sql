0a4f5e3f7e660f109a06c5ebf5388dca83e6efcd
RJ Martino <rj.martino@iprovweb.com>
2013-09-03 22:48:03 -0500

    Another commit that is broken up into multiple lines

--- firstfile.sql
+++ firstfile.sql
@@ -1,2 +1,3 @@
 SELECT rh.oeonrent, rd.pmdesc FROM renthead rh, rentdetl rd WHERE rh.kcustnum= @custid AND rh.kordnum = rd.kordnum AND rd.kpart = @kpart and rh.kordnum IN (SELECT equip.eqprano FROM equip WHERE equip.eqprntcust= @custid AND (equip.eqpstatus IN ('ON','PI') OR Col002='OTH') AND equip.GLAcc='142760') ORDER BY rh.kbranch,rh.kordnum DESC,rd.kpart,rh.oeonrent DESC
 
+This is just a test. 
